dd5eae1ca074e9284e079a18f50c57eb78378df1
Chris Filkins <filchyboy@gmail.com>
2020-09-22 16:18:32 -0700

    Worked out adding to head but it's not working correctly. But I did get a strange almost reverse list going.

--- sketch4.py
+++ sketch4.py
@@ -19,13 +19,26 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
 
     def add_to_head(self, value):
-        node = Node(value)
-        if self.head is not None:
-            node.set_next(self.head)
-        self.head = node
+        new_node = Node(value)
+        if self.head != None:
+            new_node.set_next(self.head)
+        self.head = new_node
+    
+    def remove_head(self):
+        if self.head is None:
+            return None
+        else:
+            ret_value = self.head.get_value()
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.get_next_node()
+            return ret_value
 
     def delete_list(self):
         self.head = None
@@ -52,26 +65,65 @@ class LinkedList:
         cur_node = self.head
         while cur_node.next_node != None:
             cur_node = cur_node.next_node
+            
         cur_node.next_node = new_node
 
+
+    # def append(self, value):
+    #     new_node = Node(value)
+    #     while(self.head != None):
+    #         while(self.tail != None):
+    #             cur_node = self.tail
+    #             while cur_node.next_node != None:
+    #                 cur_node = cur_node.next_node
+    #             cur_node.next_node = new_node
+    #         else:
+    #             cur_node = self.head
+    #             while cur_node.next_node != None:
+    #                 cur_node = cur_node.next_node
+    #             cur_node.next_node = new_node
+    #         return 
+
+    def reverse(self):
+        previous = None
+        current = self.head 
+        while(current != None): 
+            next_node = current.next_node
+            current.next_node = previous 
+            previous = current 
+            current = next_node
+        self.head = previous 
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        while(self.tail != None):
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
 
 ll = LinkedList()
 
-ll.add_to_head(10)
+ll.add_to_head(1111)
 print(ll.head.value)
-ll.add_to_head(12)
-print(ll.head.value)
-ll.append(100)
+
+
 ll.append(101)
-ll.append(102)
-ll.append(103)
-ll.append(104)
-ll.append(105)
-print(ll.head.next_node.value)
+
+# ll.append(102)
+
+# ll.append(103)
+
+# ll.append(104)
+
+# ll.append(105)
+
 print(ll.length())
 print(ll.display())
-ll.delete_list()
+# ll.delete_list()
+ll.reverse()
 print(ll.display())
+ll.add_to_tail(111)
+print(ll.display())
+print(ll.tail)
 
 
